1466e735ff6a31305ddc6697987f724341a88c57
Prabidhee <prabidhee@gmail.com>
2017-06-18 09:44:04 +0545

    Signed-off-by: Prabidhee <prabidhee@gmail.com>

--- app/src/main/java/com/prabidhee/www/travelnepalv2/fragment/AdventureFragment.java
+++ app/src/main/java/com/prabidhee/www/travelnepalv2/fragment/AdventureFragment.java
@@ -1,13 +1,21 @@
 package com.prabidhee.www.travelnepalv2.fragment;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.prabidhee.www.travelnepalv2.Data;
 import com.prabidhee.www.travelnepalv2.R;
+import com.prabidhee.www.travelnepalv2.Recycler_View_Adapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +33,27 @@ public class AdventureFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_adventure, container, false);
+        List<Data> data = fill_with_data();
+
+        RecyclerView recyclerView = (RecyclerView) recyclerView.findViewById();
+        Recycler_View_Adapter adapter = new Recycler_View_Adapter(data, getApplication());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
+    public List<Data> fill_with_data() {
+
+        List<Data> data = new ArrayList<>();
+
+        data.add(new Data("Batman vs Superman", "Following the destruction of Metropolis, Batman embarks on a personal vendetta against Superman ", R.drawable.ic_action_movie));
+        data.add(new Data("X-Men: Apocalypse", "X-Men: Apocalypse is an upcoming American superhero film based on the X-Men characters that appear in Marvel Comics ", R.drawable.ic_action_movie));
+        data.add(new Data("Captain America: Civil War", "A feud between Captain America and Iron Man leaves the Avengers in turmoil.  ", R.drawable.ic_action_movie));
+        data.add(new Data("Kung Fu Panda 3", "After reuniting with his long-lost father, Po  must train a village of pandas", R.drawable.ic_action_movie));
+        data.add(new Data("Warcraft", "Fleeing their dying home to colonize another, fearsome orc warriors invade the peaceful realm of Azeroth. ", R.drawable.ic_action_movie));
+        data.add(new Data("Alice in Wonderland", "Alice in Wonderland: Through the Looking Glass ", R.drawable.ic_action_movie));
+
+        return data;
+    }
+
 
 }
